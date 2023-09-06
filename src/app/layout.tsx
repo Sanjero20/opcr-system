4b9b672792602acaefd9f9087b6a7f060b9b4bdb
Sanjero20 <gelosantos72@gmail.com>
2023-09-06 23:02:55 +0800

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import '@/styles/globals.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'OPCR',
+  description: 'Group project for Advance Software Engineering',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="h-screen">{children}</body>
+    </html>
+  );
+}
