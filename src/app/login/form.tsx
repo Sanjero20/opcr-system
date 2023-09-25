72113b7e85221e4965683a292c3fa7a7867d6646
Sanjero20 <gelosantos72@gmail.com>
2023-09-25 21:58:20 +0800

    Separate login form from main component
    Separate client side from the server side components for readability and
    performance boost.

---
+++ src/app/login/form.tsx
@@ -0,0 +1,74 @@
+'use client';
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { FormEvent, useEffect, useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { CardContent, CardFooter } from '@/components/ui/card';
+import { useRouter } from 'next/navigation';
+
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  // libraries
+  const router = useRouter();
+
+  useEffect(() => {
+    if (error) setError('');
+  }, [username, password]);
+
+  const handleLogin = (e: FormEvent) => {
+    e.preventDefault();
+
+    // ! Replacing soon with real logic
+    switch (username) {
+      case 'opcr':
+      case 'ipcr':
+        router.replace('/');
+        break;
+      case 'admin':
+        router.replace('/admin');
+        break;
+      default:
+        setError('Incorrect credentials');
+    }
+  };
+
+  return (
+    <>
+      <CardContent>
+        <form
+          onSubmit={(e) => handleLogin(e)}
+          id="login-form"
+          className="flex flex-col gap-2"
+        >
+          <Input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+          />
+
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </form>
+      </CardContent>
+
+      <CardFooter className="flex flex-col justify-center gap-2">
+        <Button type="submit" form="login-form" className="w-full">
+          Sign In
+        </Button>
+
+        {error && <p className="text-destructive">*{error}*</p>}
+      </CardFooter>
+    </>
+  );
+}
+
+export default LoginForm;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,51 +1,9 @@
-'use client';
-
-import { FormEvent, useEffect, useState } from 'react';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from '@/components/ui/card';
-import useAuth from '@/stores/auth';
+
+import LoginForm from './form';
+import { Card, CardHeader } from '@/components/ui/card';
 
 function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-
-  const { updateAuth } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!error) return;
-    setError('');
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [username, password]);
-
-  const handleLogin = (e: FormEvent) => {
-    e.preventDefault();
-
-    // ! Replacing soon with real logic
-    switch (username) {
-      case 'opcr':
-      case 'ipcr':
-        updateAuth(username);
-        router.replace('/');
-        break;
-      case 'admin':
-        updateAuth(username);
-        router.replace('/admin');
-        break;
-      default:
-        setError('Incorrect credentials');
-    }
-  };
-
   return (
     <div className="background-photo flex h-screen items-center justify-center">
       <Card className="w-1/2 sm:w-1/3 xl:w-1/4">
@@ -56,35 +14,7 @@ function LoginPage() {
           </h1>
         </CardHeader>
 
-        <CardContent>
-          <form
-            onSubmit={(e) => handleLogin(e)}
-            id="login-form"
-            className="flex flex-col gap-2"
-          >
-            <Input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="Username"
-            />
-
-            <Input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-            />
-          </form>
-        </CardContent>
-
-        <CardFooter className="flex flex-col justify-center gap-2">
-          <Button type="submit" form="login-form" className="w-full">
-            Sign In
-          </Button>
-
-          {error && <p className="text-destructive">*{error}*</p>}
-        </CardFooter>
+        <LoginForm />
       </Card>
     </div>
   );
