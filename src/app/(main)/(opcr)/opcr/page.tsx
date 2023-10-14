0f49f142839ac793a351fd5027f70f177684919e
Sanjero20 <gelosantos72@gmail.com>
2023-10-14 21:10:32 +0800

    Initialize OPCR Form

--- src/app/(main)/(opcr)/opcr/page.tsx
+++ src/app/(main)/(opcr)/opcr/page.tsx
@@ -1,7 +1,13 @@
+import FormOpcr from '@/components/form-opcr/form-opcr';
+
 function OpcrPage() {
   return (
-    <div className="flex h-full flex-col">
+    <div className="flex h-full flex-col gap-2">
       <h1 className="title">OPCR FORM PREVIEW</h1>
+
+      <FormOpcr />
+
+      <div className="flex gap-2">{/* Button navigators */}</div>
     </div>
   );
 }
---
+++ src/components/form-opcr/form-opcr.tsx
@@ -0,0 +1,5 @@
+function FormOpcr() {
+  return <main className="flex-1 border border-black"></main>;
+}
+
+export default FormOpcr;
