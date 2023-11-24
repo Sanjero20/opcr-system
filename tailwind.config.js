53627962ebfce1f0980ba539cc4b9cae935efd39
Sanjero20 <gelosantos72@gmail.com>
2023-11-25 00:33:13 +0800

    Change table header color

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
     extend: {
       colors: {
         nav: '#853030',
+        tableHead: '#6A5E52',
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
