25f0541189de7d7e49c7988f0b44813447535d26
Sanjero20 <gelosantos72@gmail.com>
2023-11-24 11:30:48 +0800

    Add function to retrieve account details

--- src/services/api/admin.ts
+++ src/services/api/admin.ts
@@ -3,14 +3,25 @@ import { AccountInfo } from '@/types/response/admin';
 
 api.defaults.baseURL += '/api/admin';
 
-type AccountListReponse = {
-  data: AccountInfo[];
+type ApiResponse<T> = {
+  data: T;
   error: any;
 };
 
-export async function getAccountsList(): Promise<AccountListReponse> {
+export async function getAccountsList(): Promise<ApiResponse<AccountInfo[]>> {
   try {
     const response = await api.get('/accounts');
+    return await response.data;
+  } catch (error: any) {
+    return error.response.data;
+  }
+}
+
+export async function getAccountInfo(
+  id: string,
+): Promise<ApiResponse<AccountInfo>> {
+  try {
+    const response = await api.get(`/account/${id}`);
     return await response.data;
   } catch (error: any) {
     return error.response.data;
--- src/types/response/admin.ts
+++ src/types/response/admin.ts
@@ -1,5 +1,5 @@
 export type AccountInfo = {
-  _id: string;
+  _id: { $oid: string };
   name: string;
   email: string;
   username: string;
